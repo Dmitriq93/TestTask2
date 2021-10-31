dddffa8fe49e665b2930dbe04b8a2e3c669937ea
Dmitriy Topnikov <topnikov_93@mail.ru>
2021-10-31 19:30:35 +0300

    Настройки Webdriver получаются из файла

--- src/main/java/common/Common.java
+++ src/main/java/common/Common.java
@@ -2,26 +2,46 @@ package common;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-import static common.Config.PLATFORM_AND_BROWSER;
 import static constants.Constants.TimeoutVariable.IMPLICIT_WAIT;
 
 public class Common {
 
     public static WebDriver createDriver() {
-        WebDriver driver = null;
+        try {
+            File file = new File("webdriver.properties");
+            FileInputStream fileInput = new FileInputStream(file);
+            Properties properties = new Properties();
+            properties.load(fileInput);
+            WebDriver driver = null;
 
-        switch (PLATFORM_AND_BROWSER) {
-            case "win_chrome":
-                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-                driver = new ChromeDriver();
-                break;
-            default:
-                Assert.fail("incorrect platform" + PLATFORM_AND_BROWSER);
+            switch (properties.getProperty(Config.KEY_PROPERTIES_WEBDRIVER_USE)) {
+                case Config.VALUE_PROPERTIES_WEBDRIVER_FIREFOX:
+                    if (!properties.containsKey(Config.KEY_PROPERTIES_WEBDRIVER_PATH_FIREFOX)) {
+                        throw new Exception("text1");
+                    }
+                    System.setProperty(Config.KEY_PROPERTIES_WEBDRIVER_PATH_FIREFOX, properties.getProperty(Config.KEY_PROPERTIES_WEBDRIVER_PATH_FIREFOX));
+                    driver = new FirefoxDriver();
+                    break;
+                case Config.VALUE_PROPERTIES_WEBDRIVER_CHROME:
+                    if (!properties.containsKey(Config.KEY_PROPERTIES_WEBDRIVER_PATH_CHROME)) {
+                        throw new Exception("text2");
+                    }
+                    System.setProperty(Config.KEY_PROPERTIES_WEBDRIVER_PATH_CHROME, properties.getProperty(Config.KEY_PROPERTIES_WEBDRIVER_PATH_CHROME));
+                    driver = new ChromeDriver();
+                    break;
+                default:
+                    throw new Exception("text3");
+            }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
+            return driver;
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка",e);
         }
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
-        return driver;
     }
 }
--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -2,7 +2,10 @@ package common;
 
 public class Config {
 
-    public static final String PLATFORM_AND_BROWSER = "win_chrome";
     public static final Boolean HOLD_BROWSER_OPEN = true;
-
+    public static final String KEY_PROPERTIES_WEBDRIVER_USE = "webdriver.use.driver";
+    public static final String VALUE_PROPERTIES_WEBDRIVER_FIREFOX = "firefox";
+    public static final String VALUE_PROPERTIES_WEBDRIVER_CHROME = "chrome";
+    public static final String KEY_PROPERTIES_WEBDRIVER_PATH_FIREFOX = "webdriver.gecko.driver";
+    public static final String KEY_PROPERTIES_WEBDRIVER_PATH_CHROME = "webdriver.chrome.driver";
 }
